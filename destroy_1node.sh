a900bd5f1b67d7491f9f9fc118a4a1a8492ead18
Martijn Kieboom <martijn@kieboom.nu>
2022-02-26 14:57:30 +0100

    minor changes

--- destroy_1node.sh
+++ destroy_1node.sh
@@ -2,4 +2,4 @@
 
 # Destroy the Vagrant images
 # Launch a 1 node Confluent Platform instance using vagrant and cp-ansible
-VAGRANT_VAGRANTFILE=Vagrantfile vagrant destroy -f
+VAGRANT_VAGRANTFILE=Vagrantfile_1node vagrant destroy -f
--- ssh_1node.sh
+++ ssh_1node.sh
@@ -4,4 +4,4 @@
 # Locate the preferred template from the templates folder
 
 # Launch a 1 node Confluent Platform instance using vagrant and cp-ansible
-VAGRANT_VAGRANTFILE=Vagrantfile_1node TEMPLATE=1node.yml vagrant up
+VAGRANT_VAGRANTFILE=Vagrantfile_1node TEMPLATE=1node.yml vagrant ssh
